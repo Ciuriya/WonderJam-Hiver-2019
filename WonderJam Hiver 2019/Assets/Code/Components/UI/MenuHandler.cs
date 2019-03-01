dc86d9f210010322660a582af353e50a694ff913
Max <maxime___tremblay@hotmail.com>
2019-02-28 20:12:40 -0500

    Added some generic code to start off the project

---
+++ WonderJam Hiver 2019/Assets/Code/Components/UI/MenuHandler.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class MenuHandler : MonoBehaviour 
+{
+	[Tooltip("The currently opened menu. If null, the game is not currently paused")]
+	public GameObject m_currentMenu;
+
+	[Tooltip("The game event raised when the game is paused")]
+	public GameEvent m_pauseEvent;
+
+	[Tooltip("The game event raised when the game is resumed")]
+	public GameEvent m_resumeEvent;
+
+	[Tooltip("The game event raised when the menu changes")]
+	public GameEvent m_onMenuChangedEvent;
+	
+	private GameObject m_previousMenu; // The previously opened menu, for use with the pause menu only
+	[HideInInspector] public List<GameObject> m_openedMenus;
+
+	private MenuHandler() { }
+
+	public static MenuHandler Instance { get; private set; }
+
+	void Start() { Instance = this; }
+
+	void OnEnable() 
+	{
+		m_openedMenus = new List<GameObject>();
+		SceneManager.sceneLoaded += OnSceneLoad;
+	}
+
+	void OnDisable() 
+	{ 
+		SceneManager.sceneLoaded -= OnSceneLoad;
+	}
+
+	void Update() 
+	{
+		bool isPaused = m_currentMenu;
+
+		if(Game.m_keybinds.GetButtonDown("Pause")) GoBack();
+	}
+
+	public void GoBack() 
+	{
+		bool isPaused = m_currentMenu;
+
+		if(m_openedMenus.Count > 0 && !(m_openedMenus.Count == 1 && isPaused)) ClearMenu();
+		else if(isPaused) 
+		{
+			if(m_previousMenu) OpenMenu(m_previousMenu, true);
+			else m_resumeEvent.Raise();
+		} else m_pauseEvent.Raise();
+	}
+
+	public void OpenMenuPause(GameObject p_menu) 
+	{ 
+		OpenMenu(p_menu, true);
+	}
+
+	public void OpenMenu(GameObject p_menu) 
+	{
+		OpenMenu(p_menu, false);
+	}
+
+	public void OpenMenu(GameObject p_menu, bool p_pause) 
+	{
+		if(p_pause) 
+		{
+			if(m_currentMenu) m_currentMenu.SetActive(false);
+			if(m_previousMenu == p_menu) { m_openedMenus.Remove(m_currentMenu); m_previousMenu = null; }
+			else m_previousMenu = m_currentMenu;
+
+			m_currentMenu = p_menu;
+			m_currentMenu.SetActive(true);
+
+			if(m_previousMenu) m_openedMenus.Remove(m_previousMenu);
+			m_openedMenus.Add(p_menu);
+		} 
+		else 
+		{
+			if(m_openedMenus.Contains(p_menu)) 
+			{ 
+				p_menu.SetActive(false);
+				m_openedMenus.Remove(p_menu);
+			}
+			else 
+			{
+				p_menu.SetActive(true);
+				m_openedMenus.Add(p_menu);
+			}
+		}
+
+		m_onMenuChangedEvent.Raise();
+	}
+
+	public void ClearMenu() 
+	{
+		if(m_currentMenu) m_currentMenu.SetActive(false);
+		if(m_openedMenus.Count > 0)
+			foreach(GameObject menu in m_openedMenus)
+				menu.SetActive(false);
+
+		m_openedMenus.Clear();
+		m_currentMenu = null;
+		m_previousMenu = null;
+	}
+
+	public void ChangeScenes(string scene) 
+	{
+		SceneManager.LoadScene(scene);
+	}
+
+	public void Pause() 
+	{
+		Time.timeScale = 0f;
+	}
+
+	public void Resume() 
+	{ 
+		Time.timeScale = 1f;
+	}
+
+	void OnSceneLoad(Scene p_scene, LoadSceneMode p_mode) 
+	{ 
+		if(m_resumeEvent) m_resumeEvent.Raise();
+	}
+
+	public void Quit()
+	{ 
+		Application.Quit();
+	}
+}
---
+++ WonderJam Hiver 2019/Assets/Code/Entities/Entity.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class Entity : MonoBehaviour 
+{
+	[Tooltip("If this entity can die")]
+	public bool m_canDie;
+	[HideInInspector] public bool m_isDead;
+
+	[Tooltip("The entity's viewing variables, only set if this entity uses AI, otherwise leave it empty")]
+	public Look m_look;
+
+	[Tooltip("All runtime sets this entity is a part of")]
+	public List<EntityRuntimeSet> m_runtimeSets;
+
+	[Tooltip("Event called when the entity dies")]
+	public UnityEvent m_deathEvent;
+
+	[HideInInspector] public UnitHealth m_health;
+	[HideInInspector] public StateController m_ai;
+
+	public virtual void Awake() 
+	{
+		m_health = GetComponent<UnitHealth>();
+
+		if(m_health) m_health.Init(this);
+	}
+
+	void OnEnable() 
+	{ 
+		foreach(EntityRuntimeSet set in m_runtimeSets)
+			set.Add(this);
+	}
+
+	void OnDisable() 
+	{
+		foreach(EntityRuntimeSet set in m_runtimeSets)
+			set.Remove(this);
+	}
+
+	public void Damage(Entity p_damager, int p_damage, bool p_bypassImmunityWindow)
+	{
+		if(m_health) 
+		{
+			int finalDamage = p_damage; // calculate defense, resistances, etc. and compile it into this variable
+
+			if(finalDamage > 0) 
+			{
+				m_health.Damage(finalDamage, p_bypassImmunityWindow);
+			}
+		}
+
+		// make sure the AI starts targeting its last damager
+		if(m_ai && p_damager) m_ai.m_target = p_damager;
+	}
+
+	public void Kill() {
+		if(m_canDie && !m_isDead) {
+			m_isDead = true;
+			Die();
+		}
+	}
+
+	protected virtual void Die() {
+		Destroy(m_ai);
+
+		m_deathEvent.Invoke();
+
+		Destroy(m_health);
+		Destroy(this);
+	}
+}
+
