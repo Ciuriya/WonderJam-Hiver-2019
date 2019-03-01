ac96384a675fbdae2fb77a6d2702e1425d2894c2
Unknown <maxime_morette@hotmail.com>
2019-03-01 18:39:47 -0500

    Rajouté un sprite temporaire de triangle, créer un folder power-up dans entities et changer basebuff en abstract

--- WonderJam Hiver 2019/Assets/Code/Entities/Power-Ups/BaseBuff.cs
+++ WonderJam Hiver 2019/Assets/Code/Entities/Power-Ups/BaseBuff.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseBuff : ScriptableObject
+public abstract class BaseBuff : ScriptableObject
 {
     // Start is called before the first frame update
     void Start()
