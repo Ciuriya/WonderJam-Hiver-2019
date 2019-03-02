0bb3f91e62b405626ed55efc4517dbbcb842f9a9
nath <nathanaeltremblay19@gmail.com>
2019-03-02 06:51:17 -0500

    Player Prefab, Dev test scene ready for testing

--- WonderJam Hiver 2019/Assets/Code/Entities/Power-Ups/PowerUp.cs
+++ WonderJam Hiver 2019/Assets/Code/Entities/Power-Ups/PowerUp.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.UI;
 using UnityEngine;
 
 public abstract class PowerUp : ScriptableObject
 {
+    public Image m_icon;
+
     public abstract void Use(Shooter p_shooter);
     public abstract void End(Shooter p_shooter);
 }
