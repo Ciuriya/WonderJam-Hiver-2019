9ae62645d56d5d2821a8af559e2449633472f234
nath <nathanaeltremblay19@gmail.com>
2019-03-02 05:01:57 -0500

    Hud ui's,

---
+++ WonderJam Hiver 2019/Assets/Parrallax.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parrallax : MonoBehaviour
+{
+    public float m_ParrallaxSpeed;
+
+    public Transform[] backgrounds;//array of bakground that has to be parrallax
+    public float m_Smoothing = 1;//how smooth parralax going to be
+
+    public Transform m_BackGroundGameObject;
+    private Vector3 m_PreviousBackGroundGameObjectPos;
+
+    
+    void Start()
+    {
+        m_PreviousBackGroundGameObjectPos = m_BackGroundGameObject.position;
+
+
+        m_ParrallaxSpeed = 2;
+
+    }
+
+    void Update()
+    {
+        
+
+
+
+
+
+    }
+
+
+
+}
