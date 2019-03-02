f821dd952fa0e9922a560a4d4f53ee9a51d3e1be
Etienne <etienneemond97@hotmail.com>
2019-03-02 00:20:57 -0500

    Pour mon beau Jason

---
+++ WonderJam Hiver 2019/Assets/Code/Ennemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemy : MonoBehaviour
+{
+    public EnnemyBehavior m_behavior;
+
+
+    public void Move()
+    {
+        m_behavior.Move(transform.position);
+    }
+
+    public void Attack()
+    {
+        m_behavior.Attack();
+    }
+
+
+    void Update()
+    {
+        Move();
+    }
+}
---
+++ WonderJam Hiver 2019/Assets/Code/EnnemyBehavior.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public abstract class EnnemyBehavior
+{
+    public abstract void Move(Transform own_transform);
+
+    public abstract void Attack();
+}
