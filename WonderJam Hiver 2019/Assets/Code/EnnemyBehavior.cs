7d9257d7cb7a245b8b7cb9616d1d1fe71a730469
Etienne <etienneemond97@hotmail.com>
2019-03-02 16:22:27 -0500

    Add Enemy Movement and behavior + Game Values Managers

--- WonderJam Hiver 2019/Assets/Code/EnnemyBehavior.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[System.Serializable]
-public abstract class EnnemyBehavior
-{
-    public abstract void Move(Transform own_transform);
-
-    public abstract void Attack();
-}
---
+++ WonderJam Hiver 2019/Assets/Code/Entities/Enemy/MoveStrategy/AwayFromSpawn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "MoveStrategy/AwayFromSpawn")]
+public class AwayFromSpawn : MoveStrategy
+{
+    public float speed = .1f;
+    public float maxDistance = .3f;
+
+    public override Vector2 GetMovementVector(Enemy enemy)
+    {
+        Vector3 distance = new Vector2(enemy.transform.position.x - enemy.m_spawnFromPosition.x, 0);
+
+        return (distance.magnitude < maxDistance) ? distance.normalized * speed : new Vector3(0, 0);
+    }
+} 
