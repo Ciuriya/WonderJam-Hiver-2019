59c3de5a65d7e5c77354e11638439b8bf8089d40
Jeremy L. Riverin <darknesser77@gmail.com>
2019-03-03 02:54:06 -0500

    new script to shake the screen

---
+++ WonderJam Hiver 2019/Assets/Code/Components/Cameras/Screen_Shake.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screen_Shake : MonoBehaviour
+{
+    [Tooltip("Length of the screen tremors")]
+    public float m_shakeDuration;
+
+    [Tooltip("Strenght of the screen tremors")]
+    public float m_magnitude;
+
+    Vector3 m_initPosition;
+    Vector3 m_newPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_initPosition = gameObject.transform.localPosition;
+        m_newPosition = m_initPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_shakeDuration > 0)
+        {
+            m_newPosition.x += Random.insideUnitSphere.x * m_magnitude;
+            m_shakeDuration -= Time.deltaTime;
+            gameObject.transform.localPosition = m_newPosition;
+        }
+
+        else
+        {
+            gameObject.transform.localPosition = m_initPosition;
+            Destroy(this);
+        }
+    }
+}
+
