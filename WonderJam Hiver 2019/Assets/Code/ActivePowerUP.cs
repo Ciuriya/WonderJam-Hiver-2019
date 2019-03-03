c6f191f215ec9bb6d33960459a61b8f5d377f5d9
nath <nathanaeltremblay19@gmail.com>
2019-03-03 03:51:38 -0500

    Auto stash before revert of "Revert "Revert "Merge branch 'develop' into ft-Art"""

--- WonderJam Hiver 2019/Assets/Code/ActivePowerUP.cs
+++ WonderJam Hiver 2019/Assets/Code/ActivePowerUP.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
+﻿using UnityEngine.UI;
 using UnityEngine;
 
 public class ActivePowerUP : MonoBehaviour
 {
-    [HideInInspector] public GameObject[] m_players;
+    public int m_playerID;
 
     [HideInInspector] public Image m_Image;
 
@@ -13,7 +11,7 @@ public class ActivePowerUP : MonoBehaviour
 
     public Sprite m_UIMask;
 
-    private float m_TimeLeft;
+    private Activeplayers m_activeplayers;
 
     private Shooter m_Shooter;
 
@@ -21,34 +19,45 @@ public class ActivePowerUP : MonoBehaviour
 
     private Sprite m_sprite;
 
-   
-    void Awake()
-    {
-        m_players = GameObject.FindGameObjectsWithTag("Player");
-        m_Shooter = m_players[0].GetComponent<Shooter>();
-        m_Image = GetComponent<Image>();
-    }
+    private float m_TimeLeft;
 
     // Start is called before the first frame update
-    private void Start() { m_Image.sprite = m_UIMask; }
+    private void Start()
+    {
+            
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (m_Shooter.m_spPowerUps.Last!=null)
-        {
-            m_Image.sprite = m_Shooter.m_spPowerUps.Last.Value.shotPatternPowerUp.m_icon.sprite;
-        }
-       
-        if(m_Shooter.m_spPowerUps.Last != null)
-        {
-             m_TimeLeft = ((m_Shooter.m_spPowerUps.Last.Value.time)
-            + (m_Shooter.m_spPowerUps.Last.Value.shotPatternPowerUp.m_duration * 1000) - (Time.time * 1000)) / 1000;
-        }
 
-        if (m_TimeLeft<=0)
+
+        if (Game.m_players.m_players.Exists(p => p.m_playerId == m_playerID))
         {
+            Player player = Game.m_players.m_players.Find(p => p.m_playerId == m_playerID);
+            
+
+            m_Image = GetComponent<Image>();
             m_Image.sprite = m_UIMask;
+
+
+            if (player.m_shooter.m_spPowerUps.Last != null)
+            {
+                m_Image.sprite = player.m_shooter.m_spPowerUps.Last.Value.shotPatternPowerUp.m_icon.sprite;
+            }
+
+            if (player.m_shooter.m_spPowerUps.Last != null)
+            {
+                m_TimeLeft = ((player.m_shooter.m_spPowerUps.Last.Value.time)
+               + (player.m_shooter.m_spPowerUps.Last.Value.shotPatternPowerUp.m_duration * 1000) - (Time.time * 1000)) / 1000;
+            }
+
+            if (m_TimeLeft <= 0)
+            {
+                m_Image.sprite = m_UIMask;
+            }
+
         }
+                   
     }
 }
---
+++ WonderJam Hiver 2019/Assets/play.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play : MonoBehaviour
+{
+    public bool m_play;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ParticleSystem pS = GetComponent<ParticleSystem>();
+        pS.enableEmission = m_play;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
