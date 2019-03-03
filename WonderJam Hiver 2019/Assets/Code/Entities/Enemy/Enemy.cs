870a59c862965fda3a338769b1ed0344f058aa19
Etienne <etienneemond97@hotmail.com>
2019-03-03 13:59:35 -0500

    Add flashing on damage + BAse enemy health

--- WonderJam Hiver 2019/Assets/Code/Entities/Enemy/Enemy.cs
+++ WonderJam Hiver 2019/Assets/Code/Entities/Enemy/Enemy.cs
@@ -28,8 +28,13 @@ public class Enemy : Entity
     [HideInInspector] public Vector3 m_spawnFromPosition;
     [HideInInspector] public float m_spawnTime;
 
+    private DamageFlash flasher;
     private int totalWeight = 0;
 
+    public void Awake()
+    {
+        flasher = GetComponent<DamageFlash>();
+    }
 
     public override void OnEnable()
     {
@@ -42,7 +47,6 @@ public class Enemy : Entity
     public override void OnDamage()
     {
         base.OnDamage();
-        DamageFlash flasher = GetComponent<DamageFlash>();
 
         if (flasher)
             flasher.Flash();
